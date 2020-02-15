8a99fb9de1888fb230c14daa38feb168e2ef1f86
Your Name <you@example.com>
2020-02-15 12:59:38 +0900

    weather2

--- tenki_no_ko_project/weathermap/models.py
+++ tenki_no_ko_project/weathermap/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class weather_DB(models.Model) :
+    content = models.CharField(max_length=255)
+
--- tenki_no_ko_project/weathermap/views.py
+++ tenki_no_ko_project/weathermap/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import urllib.request
+import json
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-def index(request) :
-    return HttpResponse("안녕")
+TOKEN = '8efabdb7c4a00c765d7816f3ae2310ad'
 
+def request(url) :
+    response = urllib.request.urlopen(url)
+    byte_data = response.read()
+    text_data = byte_data.decode('utf-8')
+    return text_data
 
+def build_url(city) :
+    return f'api.openweathermap.org/data/2.5/weather?q={city}&appid={TOKEN}'
 
+@csrf_exempt
+def request_to_weather(request) :
+    json_str = ((request.body).decode('utf-8'))
+    received_json_data = json.loads(json_str)
+    userMessage = received_json_data['content'] 
+
+    url = build_url(userMessage)
+    response = request(url)
+    return json.loads(response)
